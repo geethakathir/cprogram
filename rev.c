d97f47a7933caefcd08c65f565cb926bd2f7dd5f
geethakathir <30830151+geethakathir@users.noreply.github.com>
2017-09-12 15:36:46 +0530

    Create rev.c

---
+++ rev.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int a,b,c=16;
+int main()
+{
+printf("/n enter the name");
+scanf("%d",&a);
+printf("/n enter the value");
+scanf("%d",&b);
+strrev=a,b;
+printf("/n the reversed string is");
+return 0;
+}
+
+
+
+
