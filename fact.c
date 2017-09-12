7794d6c935bf8abd105c770b58410fcbe2f1ba73
geethakathir <30830151+geethakathir@users.noreply.github.com>
2017-09-12 15:47:16 +0530

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int fact 0,n-1,1;
+int main()
+{
+printf("/n enter the number");
+scanf("%d", &n);
+printf("/n factorial of the number");
+scanf("%d",&n);
+if
+{
+fact=fact(n-1);
+}
+printf("/n the given number is factorial");
+else
+{
+printf("/n the given number is not factorial");
+}
+}
