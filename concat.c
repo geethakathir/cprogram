1adda010fcaf26501d6b3ebfd51f67f852c6e2b4
geethakathir <30830151+geethakathir@users.noreply.github.com>
2018-01-19 07:57:10 +0000

    Create concat.c

---
+++ concat.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+chae a[10],[20];
+int i=0,j=0;
+printf("enter the string");
+gets(a);
+gets(b);
+while(a[i]!='0')
+{
+i++;
+a[i]=b[j];
+i++;
+j++;
+}
+printf("\n concatneated string");
+}
+
+
+
