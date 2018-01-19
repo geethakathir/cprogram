7902c5ab694d46f98b3ea62d53d1918b04334026
geethakathir <30830151+geethakathir@users.noreply.github.com>
2018-01-19 08:52:19 +0000

    Create num90.c

---
+++ num90.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+char a[10];
+int a[10],i;
+gets(a);
+for(i=0;a[i]!=0;i++)
+{
+if(a[i]>=0&&a[i]<=9)
+{
+a[j]=a[i];
+j++;
+}
+}
+for(k=0;k<j;k++)
+{
+printf("%d",a[0]);
+}
+return 0;
+}
+
+
