2073d397dc97271a3446425ff6b3d9568268e608
geethakathir <30830151+geethakathir@users.noreply.github.com>
2018-01-21 11:42:12 +0530

    Create arr46.c

---
+++ arr46.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<math.h> 
+void main()
+{
+    int array[10];
+    int i, j, num, temp, keynum;
+    int low, mid, high;
+ 
+    printf("Enter the value of num \n");
+    scanf("%d", &num);
+    printf("Enter the elements one by one \n");
+    for (i = 0; i < num; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+    printf("Input array elements \n");
+    for (i = 0; i < num; i++)
+    {
+        printf("%d\n", array[i]);
+    }
+    printf("the sorted list of array");
+    }
+    }
