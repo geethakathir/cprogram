ad6f48c672d85f7699557c3e6446f548dde9918b
geethakathir <30830151+geethakathir@users.noreply.github.com>
2018-01-21 10:32:00 +0530

    Create odd26.c

---
+++ odd26.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,i;
+printf("enter the number");
+scanf9"%d",&a);
+if (num==odd)
+{
+printf("the number is odd");
+}
+else
+printf("thr number is even");
+}
+}
