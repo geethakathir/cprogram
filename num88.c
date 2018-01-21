6581cca1f528f4a9c36bc23d43614fda94a81c6c
geethakathir <30830151+geethakathir@users.noreply.github.com>
2018-01-21 11:36:05 +0530

    Create num88.c

---
+++ num88.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define MAX_SIZE 100
+
+int main()
+{
+    int arr[MAX_SIZE];
+    int i, n, sum=0;
+
+    
+    printf("Enter size of the array: ");
+    scanf("%d", &n);
+
+    
+    printf("Enter %d elements in the array: ", n);
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("enter the number in reverse');
+    }
+    }
+  
