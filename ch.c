f031b0f55c63128ed55c33513ea5616039e3341f
geethakathir <30830151+geethakathir@users.noreply.github.com>
2018-01-23 16:43:17 +0530

    Create ch.c

---
+++ ch.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void show voewls(int string)
+int main()
+{
+int a,b,i;
+printf("enter the total number of characters");
+scanf("%d",&a);
+if(i==null)
+{
+printf("the total characters are");
+}
+else
+{
+printf("the total characters are");
+}
+}
+}
