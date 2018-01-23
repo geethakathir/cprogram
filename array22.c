29ebef6974e8852fcfd3d9551c74c18ba4c3f920
geethakathir <30830151+geethakathir@users.noreply.github.com>
2018-01-23 16:02:02 +0530

    Create array22.c

---
+++ array22.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a[6],b[8];
+int i,j,n;
+printf(enter the number");
+for(i=0;i<n;i++)
+{
+for(j=0;j<n;j++)
+{
+scanf("%d",& a);
+}
+if(i==0)
+printf("enter the array values");
+}
+}
