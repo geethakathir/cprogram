fbfe77dcd44f433035de2617bb7259584e057007
geethakathir <30830151+geethakathir@users.noreply.github.com>
2018-01-23 16:17:43 +0530

    Create hours.c

---
+++ hours.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int convert _to minutes(int h,int m);
+int main()
+{
+int minutes(h*60)*n;
+return(total minutes);
+{
+ptintf("enter the minutes");
+scanf("%d",&a);
+printf("enter the hours");
+scanf("%d",&b);
+printf("enter the total minutes")
+convert minutes=(total minutes-convert minutes);
+}
+}
+
+
