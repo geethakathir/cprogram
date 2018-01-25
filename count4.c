f4d773cd67fdcc1ed9f20896b541cd36939a38c1
geethakathir <30830151+geethakathir@users.noreply.github.com>
2018-01-25 15:54:49 +0530

    Create count4.c

---
+++ count4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int n,i,j;
+printf("enter the number of words");
+scanf("%d",&a);
+for(i=0;i<5;i++)
+{
+for(j=0;j<5;j++)
+{
+printf("count the number of characters");
+}
+}
