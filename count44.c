adbeef317dcc905eb3c4c19cbffa2d8299d0cc56
geethakathir <30830151+geethakathir@users.noreply.github.com>
2018-01-25 16:07:56 +0530

    Create count44.c

---
+++ count44.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a,b,i;
+count=0;
+printf("enter the number");
+scanf("%d",&a);
+for(i=0;i<5;i++)
+{
+for(j=0;j<5;j++)
+{
+printf("enter the character");
+scanf("%d",&n);
+}
+}
