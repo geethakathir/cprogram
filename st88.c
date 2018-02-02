6ef80279fa5740b23aef515977ad8e1ddc4535d9
geethakathir <30830151+geethakathir@users.noreply.github.com>
2018-02-02 14:21:26 +0530

    Create st88.c

---
+++ st88.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+char a[10]="hello";
+char b[[20]="good";
+int a,b,i,j;
+for(i=0;i<=6;i++)
+{
+for(j=0;j<=6;j++)
+{
+printf("\n enter the a value");
+scanf("%d",& a);
+printf("%d", a);
+}
+}
+}
+
