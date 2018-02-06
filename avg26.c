9b5dac491153196542c117f504bd5db9a415bf8a
geethakathir <30830151+geethakathir@users.noreply.github.com>
2018-02-06 10:35:39 +0000

    Create avg26.c

---
+++ avg26.c
@@ -0,0 +1,20 @@
+  #include<stdio.h>
+ 
+  void main()
+  {
+  int i,n,sum=0,num;
+  float avg;
+  printf("Enter how many number you want\n");
+  scanf("%d",&n);
+  printf("Enter the elements one by one\n");
+  for(i=0;i<n;++i)
+   {
+  scanf("%d",&numbers);
+  sum = sum +numbers;
+   }
+  avg = sum/n;
+  printf("sum of the %d numbers = %d",n,sum);
+  printf("avg of %d numbers=%.2f",n,avg);
+  }
+ 
+ 
