102d6105eaedd4d314e4863160b08595b8bc50e6
geethakathir <30830151+geethakathir@users.noreply.github.com>
2018-02-08 11:06:44 +0000

    Create swap6.c

---
+++ swap6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+double firstNumber, secondNumber, temporaryVariable;
+printf("Enter first number ");
+scanf("%f", &firstNumber);
+printf("Enter second number ");
+scanf("%f",&secondNumber);
+printf("\nAfter swapping, firstNumber = %.2lf\n", firstNumber);
+printf("After swapping, secondNumber = %.2lf", secondNumber);
+}
