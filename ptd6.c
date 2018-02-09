7364210ac542c4209fe7cd0fe890d22ca6fa3355
geethakathir <30830151+geethakathir@users.noreply.github.com>
2018-02-09 03:03:13 -0600

    Create ptd6.c

---
+++ ptd6.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int num;
+printf("Enter an integer: ");
+scanf("%d", &num);
+if(number % 2 == 0)
+printf("%d is even.", number);
+else
+printf("%d is odd.", number);
+}
+
