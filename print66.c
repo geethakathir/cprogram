b18537acad8daed4332ad634fea0971dd214e6be
geethakathir <30830151+geethakathir@users.noreply.github.com>
2018-02-09 02:46:24 -0600

    Create print66.c

---
+++ print66.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int num,temp,digit,sum=0;
+printf("enter the number");
+scanf("%d",&a);
+temp=num;
+while(n>0)
+{
+digit=num%10;
+sum=sum+digit;
+num/=10;
+printf("Given number = %ld\n", temp);
+printf("Sum of the digits %ld = %ld\n", temp, sum);
+}
+}
