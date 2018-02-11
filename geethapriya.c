b9c8bb5b8397fe761e26fc6b1bbb64969221022d
geethakathir <30830151+geethakathir@users.noreply.github.com>
2018-02-11 11:09:41 +0530

    Create geethapriya.c

---
+++ geethapriya.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void move(int)
+void main()
+{
+    int speed;
+    printf("\n enter the speed");
+    scanf("%d",&speed);
+    move(speed);
+}
+void move (int speed)
+{
+    printf("\n enter the moving speed of %d",speed);
+    printf("\n the obstacles is ");
+    scanf("%d",&speed);
+    switch(ch)
+    {
+        case 1:printf("\n the car stops");
+        break;
+        case 2:printf("\n the car moves slowly");
+        break;
+        case 3:printf("\n the car is moving"):
+        break;
+    }
+    }
