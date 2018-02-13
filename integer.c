7a05eaa7bd553f8b47ae82e08d376f73ce35cf20
geethakathir <30830151+geethakathir@users.noreply.github.com>
2018-02-13 15:44:18 +0530

    Create integer.c

---
+++ integer.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int n,i,j;
+printf("\n enter the number"):
+scanf("%d",&n);
+for(i=0;i<=5;i++)
+{
+for(j=0;j<=5;j++)
+{
+printf("\n the value is");
+}
+}
+}
+
+
+
