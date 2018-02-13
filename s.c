a805d20e773a300c82d47be0a2fbe4627f3c0555
geethakathir <30830151+geethakathir@users.noreply.github.com>
2018-02-13 16:30:06 +0530

    Create s.c

---
+++ s.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a[10],b[10],i;
+printf("\n enter the number");
+scanf("%d",&a[i]);
+printf("\n enter the number");
+scanf("%d",&b[j]);
+if(a[i] && a[j])
+{
+printf("\n swap a[i]);
+}
+else
+{
+printf("\n swap b[i]);
+}
+}
