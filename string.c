62b9960cc325c063501d01cd6ed5dfeec95a7c37
geethakathir <30830151+geethakathir@users.noreply.github.com>
2018-02-13 15:55:25 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+int a;
+printf("\n enter the number");
+scanf("%s",&a);
+if(s[1]==s[2])
+{
+printf("\n the entered string is");
+}
+else
+{
+printf("\n the entered string is not equal");
+}
+}
