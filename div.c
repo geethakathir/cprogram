71e0a1b918d74710497f33be766f7ef458133a4a
geethakathir <30830151+geethakathir@users.noreply.github.com>
2018-02-22 16:26:30 +0530

    Create div.c

---
+++ div.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a[10],i;
+printf("\n enter the number");
+scanf("%d",& a);
+if(a[i]==2)
+{
+printf("\n the number is divisible");
+}
+else
+{
+printf("\n the number is not divisible");
+}
+}
