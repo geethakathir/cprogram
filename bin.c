fe16952eef9254320371cbc7698ca14aaa39a3e6
geethakathir <30830151+geethakathir@users.noreply.github.com>
2018-02-22 16:38:00 +0530

    Create bin.c

---
+++ bin.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a[10],i,j;
+printf("\n enter the string");
+scanf("%s",&a);
+for(i=0;i<=n;i++)
+{
+for(j=0;j<=n;j++)
+{
+printf("%d",binary representation");
+}
+}
+}
