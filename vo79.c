0f952a084faf789939fd96e8e7d283964f0099c0
geethakathir <30830151+geethakathir@users.noreply.github.com>
2018-02-27 16:48:35 +0530

    Create vo79.c

---
+++ vo79.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[10]="string";
+    int i;
+    for(i=0;a[i]!=0;i++)
+    {
+    if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u')
+    
+     printf("YES");
+    }
+}
+  
