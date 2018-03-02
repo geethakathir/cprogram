9c5df55c9354985d92ad335bc7911c02354e0c73
geethakathir <30830151+geethakathir@users.noreply.github.com>
2018-03-02 09:04:35 +0000

    Create multii.c

---
+++ multii.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+  int num,i,count=0;
+  printf("Enter the number");
+  scanf("%d",&num);
+  if(num%13==0)
+  {
+    printf("YES");
+  }
+  else
+  {
+    printf("NO");
+    }
+    
+}
